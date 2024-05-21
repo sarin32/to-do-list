74ac8eac8c688839a379ec4a9bb8d26fcb67aca5
sarin32 <execode02@gmail.com>
2024-05-21 21:33:52 +0530

    [feat] created errors

---
+++ backend/src/app/errors/index.js
@@ -0,0 +1,54 @@
+import HttpStatusCode from '../config/index.js';
+
+export class BaseError extends Error {
+  constructor({ statusCode, message, isToBeReported, errorCode }) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errorCode = errorCode;
+    this.isToBeReported = isToBeReported;
+  }
+}
+
+export class BadRequestError extends BaseError {
+  constructor(message, errorCode) {
+    super({
+      message,
+      errorCode,
+      isToBeReported: false,
+      statusCode: HttpStatusCode.BAD_REQUEST,
+    });
+  }
+}
+
+export class AuthorizationError extends BaseError {
+  constructor(message, errorCode) {
+    super({
+      message,
+      errorCode,
+      isToBeReported: false,
+      statusCode: HttpStatusCode.UNAUTHORIZED,
+    });
+  }
+}
+
+export class ConflictError extends BaseError {
+  constructor(message, errorCode) {
+    super({
+      message,
+      errorCode,
+      isToBeReported: false,
+      statusCode: HttpStatusCode.CONFLICT,
+    });
+  }
+}
+
+export class ForbiddenError extends BaseError {
+  constructor(message, errorCode) {
+    super({
+      message,
+      errorCode,
+      isToBeReported: false,
+      statusCode: HttpStatusCode.FORBIDDEN,
+    });
+  }
+}
