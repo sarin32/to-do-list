b4e46ba6b31e30d26baead4a71dce55c4e3bd7c5
sarin32 <execode02@gmail.com>
2024-05-22 20:48:09 +0530

    [fix] client creation issue fix

--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -3,21 +3,18 @@ import { DATABASE_TO_DO_LIST, DATABASE_SETTINGS } from '../config/index.js';
 
 let client;
 
+const createClient = () => {
+  client = new MongoClient(DATABASE_SETTINGS.URL);
+}
+
+createClient()
+
 export const startConnection = async () => {
   if (!client) {
-    client = new MongoClient(DATABASE_SETTINGS.URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
     await client.connect();
   }
 };
 
 export const getCollection = collectionName => {
-  if (!client) {
-    throw new Error(
-      'Client is not connected. Please call startConnection first.'
-    );
-  }
   return client.db(DATABASE_TO_DO_LIST).collection(collectionName);
 };
