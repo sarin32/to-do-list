2e5c874c42ba27e2875407a9da9a47256902bc75
sarin32 <execode02@gmail.com>
2024-05-23 20:16:11 +0530

    [feat] exported repositories

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -1,1 +1,3 @@
 export * as connection from './connection.js'
+export * as emailVerificationRepository from './repository/email-verification.repository.js'
+export * as userRepository from './repository/user.repository.js' 
